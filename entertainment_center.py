06e09574c977ba5e5512eff84bb530319a0ba6b7
arunpenmetsa <arunpenmetsa@gmail.com>
2015-10-03 18:44:17 -0700

    Style Cleanup

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,47 +1,67 @@
 import media
 import fresh_tomatoes
-from operator import itemgetter, attrgetter, methodcaller
+from operator import attrgetter
 
 # Create the Movie objects for titles to be displayed
 
-shawshank_redemption = media.Movie("Shawshank Redemption",
-                        "A story of an innocent man in prison",
-                        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-                        "https://www.youtube.com/watch?v=6hB3S9bIaco","R", "1994")
+shawshank_redemption = media.Movie(
+        "Shawshank Redemption",
+        "A story of an innocent man in prison",
+        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",  # noqa
+        "https://www.youtube.com/watch?v=6hB3S9bIaco",
+        "R",
+        "1994")
 
-tora_tora_tora = media.Movie("Tora Tora Tora",
-                        "The attack on Pearl Harbor during World War II",
-                        "https://upload.wikimedia.org/wikipedia/en/1/1a/ToraToraTora1970.png",
-                        "https://www.youtube.com/watch?v=GgX1WBf2SjE","G", "1970")
+tora_tora_tora = media.Movie(
+        "Tora Tora Tora",
+        "The attack on Pearl Harbor during World War II",
+        "https://upload.wikimedia.org/wikipedia/en/1/1a/ToraToraTora1970.png",  # noqa
+        "https://www.youtube.com/watch?v=GgX1WBf2SjE",
+        "G",
+        "1970")
 
-guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy", 
-                        "Extraterrestrial Misfits fighting over a powerful artifact",
-                        "https://upload.wikimedia.org/wikipedia/en/8/8f/GOTG-poster.jpg",
-                        "https://www.youtube.com/watch?v=b3isCLVghoI","PG-13", "2014")
+guardians_of_the_galaxy = media.Movie(
+        "Guardians of the Galaxy",
+        "Extraterrestrial Misfits fighting over a powerful artifact",
+        "https://upload.wikimedia.org/wikipedia/en/8/8f/GOTG-poster.jpg",  # noqa
+        "https://www.youtube.com/watch?v=b3isCLVghoI",
+        "PG-13",
+        "2014")
 
-return_of_the_king = media.Movie("Return of the King", 
-                        "Epic journey to destroy the one ring and evil on Middle Earth",
-                        "https://upload.wikimedia.org/wikipedia/en/9/9d/Lord_of_the_Rings_-_The_Return_of_the_King.jpg",
-                        "https://www.youtube.com/watch?v=WIrRJ8bCZYQ","PG-13","2003")
+return_of_the_king = media.Movie(
+        "Return of the King",
+        "Epic journey to destroy the one ring and evil on Middle Earth",
+        "https://upload.wikimedia.org/wikipedia/en/9/9d/Lord_of_the_Rings_-_The_Return_of_the_King.jpg",  # noqa
+        "https://www.youtube.com/watch?v=WIrRJ8bCZYQ",
+        "PG-13",
+        "2003")
 
-alien = media.Movie("Alien", "Space Marines fight a deadly alien organism",
-                        "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
-                        "https://www.youtube.com/watch?v=LjLamj-b0I8","R", "1979")
+alien = media.Movie(
+        "Alien",
+        "Space Marines fight a deadly alien organism",
+        "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",  # noqa
+        "https://www.youtube.com/watch?v=LjLamj-b0I8",
+        "R",
+        "1979")
 
-gladiator = media.Movie("Gladiator", 
-                        "A Roman general is betrayed by the new Emperor",
-                        "https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg",
-                        "https://www.youtube.com/watch?v=uwTKRz-WmHU","R", "2000")
+gladiator = media.Movie(
+        "Gladiator",
+        "A Roman general is betrayed by the new Emperor",
+        "https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg",  # noqa
+        "https://www.youtube.com/watch?v=uwTKRz-WmHU",
+        "R",
+        "2000")
 
 # Create the list of movies
-movies = [shawshank_redemption, tora_tora_tora, guardians_of_the_galaxy, return_of_the_king, 
-                alien, gladiator]
+movies = [shawshank_redemption,
+          tora_tora_tora,
+          guardians_of_the_galaxy,
+          return_of_the_king,
+          alien,
+          gladiator]
 
-# Sort the list to show oldest movies first 
-movies_sort= sorted(movies, key=attrgetter('release_year'))
+# Sort the list to show oldest movies first
+movies_sort = sorted(movies, key=attrgetter('release_year'))
 
 # Display the library
 fresh_tomatoes.open_movies_page(movies_sort)
-
-#print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__doc__)
--- media.py
+++ media.py
@@ -2,35 +2,42 @@ import webbrowser
 from datetime import date
 
 # Each class has a __doc__ variable
-
 # The movie class defines the title, storyline, image, trailer, rating
 # and the release year for the movie
 
+
 class Movie():
     """ This classs provides a way to store movie related information """
-    
+
     # A list of all valid ratings
-    VALID_RATINGS = ["G","PG","PG-13","R"]
-    
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
     # Init function initializes all the variables
-    def __init__(self, movie_title, movie_storyline,
-                 poster_image,trailer_youtube, movie_rating, movie_release_year):   # self is the instance variable that is created    
+    # self is the instance variable that is created
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube,
+                 movie_rating,
+                 movie_release_year):
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
+
         # Check to make sure the movie rating is an accepted rating
         # else display a not available message
         try:
-            b=Movie.VALID_RATINGS.index(movie_rating)
+            b = Movie.VALID_RATINGS.index(movie_rating)
         except ValueError:
             self.rating = "NA"
         else:
             self.rating = movie_rating
 
         # Check to make sure the release year makes sense
-        if int(movie_release_year) > date.today().year or int(movie_release_year) < 1896: 
+        if (int(movie_release_year) > date.today().year or int(movie_release_year) < 1896):  # noqa
             self.release_year = "NA"
         else:
             self.release_year = movie_release_year
